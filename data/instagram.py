6a3f1af45e2d4a764e318a8ba9e1d2af9ff84e80
thundermms <thundermms123@gmail.com>
2021-08-31 21:44:23 +0800

    added json to csv

--- data/instagram.py
+++ data/instagram.py
@@ -1,8 +1,10 @@
 import pandas as pd
 
-# df = pd.read_json(r'instagram.json')
+df = pd.read_json(r'instagram_new.json')
+df2=pd.read_csv(r'instagram.csv')
 # print(df.shape[0])
-# df.to_csv (r'instagram.csv', index = None)
+df.to_csv (r'instagram_new.csv', index = None)
+df.append(df2)
 
-df = pd.read_csv('yelp.csv')
-df.to_json('restaurant.json', orient='records')
+# df = pd.read_csv('yelp.csv')
+# df.to_json('restaurant.json', orient='records')
